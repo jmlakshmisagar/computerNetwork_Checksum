78b4f96f4d0b3758997d397a69a029e1c797713a
Lakshmisagar J M <152427404+jmlakshmisagar@users.noreply.github.com>
2024-03-26 00:24:10 +0530

    checksum.py

--- checksum.py
+++ checksum.py
@@ -1,33 +1,61 @@
 print("Check sum calcuator")
-n = int(input("Enter no. of frames:"))
-l = []
-sum = ""
-for i in range(n):
-  l.append(input(f"Enter frame {i+1}:"))
-if n<=0:
-  print("No frames to check")
-elif n==1:
-  checksum = "".join([str(int(not int(bit))) for bit in l[0]])
-  print("Check sum:", checksum)
-else:
-  for i in range(1, n):
-    print(" ", l[i - 1])
-    print("+", l[i])
-    print("-----------")
-    sum = str(bin(int(l[i - 1], 2) + int(l[i], 2)))[2:]
-    l[i] = ((len(l[0])-len(sum))*"0")+sum
-    print(" ", sum)
-    print()
-  
-    if len(str(l[0])) < len(str(sum)):
-      print(" ", sum[1:])
-      print("+", (len(str(l[i])) - 3) * " ", "1")
+s = []
+r = []
+
+
+def inputEl(n, frames):
+  for i in range(n):
+    frames.append(input(f"Enter frame{i+1}: "))
+
+
+def calculate(frames):
+  n = len(frames)
+  result = ""
+  if n <= 0:
+    print("No frames to check")
+  elif n == 1:
+    return frames[0]
+  else:
+    for i in range(1, n):
+      print(" ", frames[i - 1])
+      print("+", frames[i])
       print("-----------")
-      sum = str(bin(int("1", 2) + int(sum[1:], 2)))[2:]
-      l[i] = ((len(l[0])-len(sum))*"0")+sum
-      print(" ", sum)
+      result = str(bin(int(frames[i - 1], 2) + int(frames[i], 2)))[2:]
+      frames[i] = ((len(frames[0]) - len(result)) * "0") + result
+      print(" ", frames[i])
       print()
-  checksum=((len(l[0])-len(sum))*"0")+sum
-  checksum = "".join([str(int(not int(bit))) for bit in checksum])
-  print("Check sum:", checksum)
-print("Receiver code : ")
+
+      if len(str(frames[0])) < len(str(result)):
+        print(" ", result[1:])
+        print("+", (len(str(frames[i])) - 3) * " ", "1")
+        print("-----------")
+        result = str(bin(int("1", 2) + int(result[1:], 2)))[2:]
+        frames[i] = ((len(frames[0]) - len(result)) * "0") + result
+        print(" ", frames[i])
+        print()
+    return ((len(frames[0]) - len(result)) * "0") + result
+
+
+print("\n--Enter sender frames(assuming all frames are received)--\n")
+n = int(input("Enter no. of frames:"))
+inputEl(n, s)
+Ssum = calculate(s)
+checksum = "".join([str(int(not int(bit))) for bit in Ssum])
+print("Sender chechsum is:", checksum)
+
+print("\n--Enter receiver frames details--\n")
+inputEl(n, r)
+Rsum = calculate(r)
+print("Final checksum calculation:")
+print(" ", checksum)
+print("+", Rsum)
+print("-----------")
+final = str(bin(int(checksum, 2) + int(Rsum, 2)))[2:]
+print(" ", final)
+validate = "".join([str(int(not int(bit))) for bit in final])
+print("Receiver checksum is:", validate)
+
+if "1" in validate:
+  print("\nError detected")
+else:
+  print("\nNo error detected")
